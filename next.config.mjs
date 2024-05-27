30d0e3894e5a1461ab3063972311ef79bc517da8
MT <zuitao53@gmail.com>
2024-05-26 21:23:51 -0700

    fix config bug

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
-
 const isProd = process.env.NODE_ENV === 'production';
 
-module.exports = {
+const nextConfig = {
   basePath: isProd ? '/realmichaeltao.github.io' : '',
   assetPrefix: isProd ? '/realmichaeltao.github.io/' : '',
 };
 
+export default nextConfig;
