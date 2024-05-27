27fcd5c7d3191173bf72e31890d303c680bd5407
MT <zuitao53@gmail.com>
2024-05-26 21:14:42 -0700

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -2,3 +2,11 @@
 const nextConfig = {};
 
 export default nextConfig;
+
+const isProd = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  basePath: isProd ? '/realmichaeltao.github.io' : '',
+  assetPrefix: isProd ? '/realmichaeltao.github.io/' : '',
+};
+
